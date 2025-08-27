9242099fb51a687d4e82cab5cda71e4a2b7eb947
BrianPiedad1 <brianpiedad123456@gmail.com>
2025-08-27 09:46:37 +0800

    Firebase config

---
+++ app/firebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyC4enNSpa5dRLJ4tpKLcBjhvFWdNLV6nAo",
+  authDomain: "bpp-goals.firebaseapp.com",
+  projectId: "bpp-goals",
+  storageBucket: "bpp-goals.firebasestorage.app",
+  messagingSenderId: "1025190612447",
+  appId: "1:1025190612447:web:08a6e775f305690ae1086a"
+};
+
+const app = initializeApp(firebaseConfig);
